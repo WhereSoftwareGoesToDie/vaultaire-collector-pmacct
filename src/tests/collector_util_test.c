81154157e5fe17c9606ec38f1405eb74fe5c6e70
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-07-23 14:09:58 +1000

    fixed tests

--- src/tests/collector_util_test.c
+++ src/tests/collector_util_test.c
@@ -4,17 +4,17 @@
 
 #include <util.h>
 
-void test_build_source() {
+void test_build_address_string() {
 	char *collection_point = "syd1";
 	char *ip = "127.0.0.1";
 	const char *bytes = "tx";
-	char *res = build_source(collection_point, ip, bytes);
+	char *res = build_address_string(collection_point, ip, bytes);
 	g_assert_cmpstr(res, ==, "bytes:tx,collection_point:syd1,ip:127.0.0.1,");
 	return;
 }
 
 int main(int argc, char **argv) {
 	g_test_init(&argc, &argv, NULL);
-	g_test_add_func("/build_source/build_source", test_build_source);
+	g_test_add_func("/build_address_string/build_address_string", test_build_address_string);
 	return g_test_run();
 }
