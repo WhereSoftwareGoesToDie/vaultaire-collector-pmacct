f06bdb902e5d2a884776e7467584ce68f7fa869d
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-07-23 16:14:50 +1000

    indentation now matches style guide

--- src/collector.c
+++ src/collector.c
@@ -11,8 +11,8 @@
 #include "util.h"
 
 /* Max time to wait between batching up frames to send to voltaire */
-#define BATCH_PERIOD    0.1
-#define DEFAULT_LIBMARQUISE_ORIGIN    "BENHUR"
+#define BATCH_PERIOD	0.1
+#define DEFAULT_LIBMARQUISE_ORIGIN	"BENHUR"
 
 
 #define __STRINGIZE(x) #x
@@ -48,84 +48,84 @@ GTree *sd_hashes;
  * and we have no other timestamp source
  */
 uint64_t timestamp_now() {
-    struct timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts)) { perror("clock_gettime"); exit(2); }
-    return (ts.tv_sec*1000000000) + ts.tv_nsec;
+	struct timespec ts;
+	if (clock_gettime(CLOCK_REALTIME, &ts)) { perror("clock_gettime"); exit(2); }
+	return (ts.tv_sec*1000000000) + ts.tv_nsec;
 }
 
 typedef struct {
-    in_addr_t network;
-    in_addr_t netmask;
-    void * next;
+	in_addr_t network;
+	in_addr_t netmask;
+	void * next;
 } networkaddr_ll_t;
 
 /* returns 1 if it is in the whitelist, 0 if it is not, or -1
  * if it is not a well-formed address
  */
 static inline int is_address_in_whitelist(char *ipaddr , networkaddr_ll_t *whitelist) {
-    struct in_addr addr;
-    if (inet_aton(ipaddr, &addr) == 0) return -1;
-    /* everything is whitelisted by default if there is no whitelist */
-    if (whitelist == NULL) return 1;
-    for (; whitelist != NULL; whitelist = whitelist->next) {
-        if ((addr.s_addr & whitelist->netmask) == whitelist->network)
-            return 1;
-    }
-    return 0;
+	struct in_addr addr;
+	if (inet_aton(ipaddr, &addr) == 0) return -1;
+	/* everything is whitelisted by default if there is no whitelist */
+	if (whitelist == NULL) return 1;
+	for (; whitelist != NULL; whitelist = whitelist->next) {
+		if ((addr.s_addr & whitelist->netmask) == whitelist->network)
+			return 1;
+	}
+	return 0;
 }
 static void free_whitelist(networkaddr_ll_t *whitelist) {
-    while (whitelist != NULL) {
-        networkaddr_ll_t *next = whitelist->next;
-        free(whitelist);
-        whitelist = next;
-    }
+	while (whitelist != NULL) {
+		networkaddr_ll_t *next = whitelist->next;
+		free(whitelist);
+		whitelist = next;
+	}
 }
 
 networkaddr_ll_t * read_ip_whitelist(char *pathname) {
-    char *network = NULL;
-    char *netmask = NULL;
-    networkaddr_ll_t *head = NULL;
+	char *network = NULL;
+	char *netmask = NULL;
+	networkaddr_ll_t *head = NULL;
 
-    FILE *infile = fopen(pathname, "r");
-    if (infile == NULL)
-        return perror(pathname), NULL;
+	FILE *infile = fopen(pathname, "r");
+	if (infile == NULL)
+		return perror(pathname), NULL;
 
-    while (fscanf(infile,
-            " %" SCANF_ALLOCATE_STRING_FLAG "[0123456789.]"
-            "/" SCANF_ALLOCATE_STRING, &network,&netmask) == 2) {
-        networkaddr_ll_t * entry = malloc(sizeof(networkaddr_ll_t));
-        if (entry == NULL)
-            return perror("malloc"),NULL;
-        struct in_addr a;
-        if (inet_aton(network, &a) == 0) {
-            fprintf(stderr,"Invalid whitelisted network '%s'. Skipping\n",network);
-            free(entry); free(network); free(netmask); continue;
-        }
-        entry->network = a.s_addr;
-        struct in_addr b;
-        if (inet_aton(netmask, &b) == 0) {
-            fprintf(stderr,"Invalid whitelisted network '%s'. Skipping\n",network);
-            free(entry); free(network); free(netmask); continue;
-        }
-        entry->netmask = b.s_addr;
-        if (ntohl( entry->netmask ) <= 24) {
-            /* Prefix length not netmask */
-            entry->netmask = ntohl(entry->netmask);
-            entry->netmask = ~((1<<(32- entry->netmask )) - 1);
-            entry->netmask = htonl(entry->netmask);
-        }
-        a.s_addr = entry->network;
-        b.s_addr = entry->netmask;
-        fprintf(stderr,"Added to whitelist: %s/",
-                inet_ntoa(a));
-        fprintf(stderr,"%s\n",inet_ntoa(b));
+	while (fscanf(infile,
+			" %" SCANF_ALLOCATE_STRING_FLAG "[0123456789.]"
+			"/" SCANF_ALLOCATE_STRING, &network,&netmask) == 2) {
+		networkaddr_ll_t * entry = malloc(sizeof(networkaddr_ll_t));
+		if (entry == NULL)
+			return perror("malloc"),NULL;
+		struct in_addr a;
+		if (inet_aton(network, &a) == 0) {
+			fprintf(stderr,"Invalid whitelisted network '%s'. Skipping\n",network);
+			free(entry); free(network); free(netmask); continue;
+		}
+		entry->network = a.s_addr;
+		struct in_addr b;
+		if (inet_aton(netmask, &b) == 0) {
+			fprintf(stderr,"Invalid whitelisted network '%s'. Skipping\n",network);
+			free(entry); free(network); free(netmask); continue;
+		}
+		entry->netmask = b.s_addr;
+		if (ntohl( entry->netmask ) <= 24) {
+			/* Prefix length not netmask */
+			entry->netmask = ntohl(entry->netmask);
+			entry->netmask = ~((1<<(32- entry->netmask )) - 1);
+			entry->netmask = htonl(entry->netmask);
+		}
+		a.s_addr = entry->network;
+		b.s_addr = entry->netmask;
+		fprintf(stderr,"Added to whitelist: %s/",
+				inet_ntoa(a));
+		fprintf(stderr,"%s\n",inet_ntoa(b));
 
-        entry->next = head;
-        head = entry;
-        free(network); free(netmask);
-    }
-    fclose(infile);
-    return head;
+		entry->next = head;
+		head = entry;
+		free(network); free(netmask);
+	}
+	fclose(infile);
+	return head;
 }
 
 /*K
@@ -140,161 +140,161 @@ networkaddr_ll_t * read_ip_whitelist(char *pathname) {
  * returns >= 1 on success
  */
 int parse_pmacct_record(char *cs, char **source_ip, char **dest_ip, uint64_t *bytes) {
-    /* Format (with more whitespace in actual input):
-     *
-     * ID CLASS SRC_MAC DST_MAC VLAN SRC_AS DST_AS SRC_IP DST_IP SRC_PORT DST_PORT TCP_FLAGS PROTOCOL TOS PACKETS FLOWS BYTES
-     * 0 unknown 00:00:00:00:00:00 00:00:00:00:00:00 0 0 0 202.4.228.250 180.76.5.15 0 0 0 ip 0 24 0 34954
-     *
-     * We ignore everything other than source IP, destination IP, and bytes
-     */
-    *source_ip = NULL;
-    *dest_ip = NULL;
-    return sscanf(cs,
-        "%*s%*s%*s%*s%*s%*s%*s"
-        SCANF_ALLOCATE_STRING
-        SCANF_ALLOCATE_STRING
-        "%*s%*s%*s%*s%*s%*s%*s"
-        "%lu",
-        source_ip, dest_ip, bytes
-        ) == 3;
+	/* Format (with more whitespace in actual input):
+	 *
+	 * ID CLASS SRC_MAC DST_MAC VLAN SRC_AS DST_AS SRC_IP DST_IP SRC_PORT DST_PORT TCP_FLAGS PROTOCOL TOS PACKETS FLOWS BYTES
+	 * 0 unknown 00:00:00:00:00:00 00:00:00:00:00:00 0 0 0 202.4.228.250 180.76.5.15 0 0 0 ip 0 24 0 34954
+	 *
+	 * We ignore everything other than source IP, destination IP, and bytes
+	 */
+	*source_ip = NULL;
+	*dest_ip = NULL;
+	return sscanf(cs,
+		"%*s%*s%*s%*s%*s%*s%*s"
+		SCANF_ALLOCATE_STRING
+		SCANF_ALLOCATE_STRING
+		"%*s%*s%*s%*s%*s%*s%*s"
+		"%lu",
+		source_ip, dest_ip, bytes
+		) == 3;
 }
 
 static inline int emit_bytes(marquise_ctx *ctx, char *address_string, 
-        marquise_source *marq_source, uint64_t timestamp, uint64_t bytes) {
-    uint64_t address = marquise_hash_identifier(address_string, strlen(address_string));
-    char *hash_string = serialise_marquise_source(marq_source);
-    uint64_t hash = marquise_hash_identifier(hash_string, strlen(hash_string));
-    int simple_success;
-    int sd_success;
-    simple_success = marquise_send_simple(ctx, address, timestamp, bytes);
-    if (simple_success != 0) {
-        DEBUG_PRINTF("successfully queued packet\n");
-    } else {
-        DEBUG_PRINTF("failed to send packet\n");
-    }    
-    if (g_tree_lookup(sd_hashes, (gpointer)&hash) == NULL) {
-        int herpaderp = 1; //Dummy value, could be anything not NULL
-        g_tree_insert(sd_hashes, (gpointer)&hash, (gpointer)&herpaderp);
-        sd_success = marquise_update_source(ctx, address, marq_source);
-        if (sd_success != 0) {
-            DEBUG_PRINTF("successfully queued source packet\n");
-        } else {
-            DEBUG_PRINTF("failed to send source packet\n");
-        }    
-    }
-    return simple_success;
+		marquise_source *marq_source, uint64_t timestamp, uint64_t bytes) {
+	uint64_t address = marquise_hash_identifier(address_string, strlen(address_string));
+	char *hash_string = serialise_marquise_source(marq_source);
+	uint64_t hash = marquise_hash_identifier(hash_string, strlen(hash_string));
+	int simple_success;
+	int sd_success;
+	simple_success = marquise_send_simple(ctx, address, timestamp, bytes);
+	if (simple_success != 0) {
+		DEBUG_PRINTF("successfully queued packet\n");
+	} else {
+		DEBUG_PRINTF("failed to send packet\n");
+	}	
+	if (g_tree_lookup(sd_hashes, (gpointer)&hash) == NULL) {
+		int herpaderp = 1; //Dummy value, could be anything not NULL
+		g_tree_insert(sd_hashes, (gpointer)&hash, (gpointer)&herpaderp);
+		sd_success = marquise_update_source(ctx, address, marq_source);
+		if (sd_success != 0) {
+			DEBUG_PRINTF("successfully queued source packet\n");
+		} else {
+			DEBUG_PRINTF("failed to send source packet\n");
+		}	
+	}
+	return simple_success;
 }
 
 static inline int emit_tx_bytes(marquise_ctx *ctx,
-        char *collection_point, char *ip, uint64_t timestamp,
-        uint64_t bytes) {
-    char *address_string = build_address_string(collection_point, ip, "tx");
-    marquise_source *marq_source = build_marquise_source(collection_point, ip, "tx");
-    int ret = emit_bytes(ctx, address_string, marq_source, timestamp, bytes);
-    free(address_string);
-    return ret;
+		char *collection_point, char *ip, uint64_t timestamp,
+		uint64_t bytes) {
+	char *address_string = build_address_string(collection_point, ip, "tx");
+	marquise_source *marq_source = build_marquise_source(collection_point, ip, "tx");
+	int ret = emit_bytes(ctx, address_string, marq_source, timestamp, bytes);
+	free(address_string);
+	return ret;
 }
 
 static inline int emit_rx_bytes(marquise_ctx *ctx,
-        char *collection_point, char *ip, uint64_t timestamp,
-        uint64_t bytes) {
-    char *address_string = build_address_string(collection_point, ip, "rx");
-    marquise_source *marq_source = build_marquise_source(collection_point, ip, "rx");
-    int ret = emit_bytes(ctx, address_string, marq_source, timestamp, bytes);
-    free(address_string);
-    return ret;
+		char *collection_point, char *ip, uint64_t timestamp,
+		uint64_t bytes) {
+	char *address_string = build_address_string(collection_point, ip, "rx");
+	marquise_source *marq_source = build_marquise_source(collection_point, ip, "rx");
+	int ret = emit_bytes(ctx, address_string, marq_source, timestamp, bytes);
+	free(address_string);
+	return ret;
 }
 
 gint hash_comp(gconstpointer a, gconstpointer b) {
-    return *(uint64_t*)a - *(uint64_t*)b;
+	return *(uint64_t*)a - *(uint64_t*)b;
 }
 
 int main(int argc, char **argv) {
-    FILE *infile;
-    char buf[BUFSIZ];
-    char *source_ip;
-    char *dest_ip;
-    uint64_t bytes;
-    uint64_t timestamp;
-    uint64_t last_timestamp;
-    char *collection_point;
-    marquise_ctx *ctx;
-    networkaddr_ll_t * ip_whitelist = NULL;
+	FILE *infile;
+	char buf[BUFSIZ];
+	char *source_ip;
+	char *dest_ip;
+	uint64_t bytes;
+	uint64_t timestamp;
+	uint64_t last_timestamp;
+	char *collection_point;
+	marquise_ctx *ctx;
+	networkaddr_ll_t * ip_whitelist = NULL;
 
-    sd_hashes = g_tree_new(hash_comp);
-    if (argc < 3) {
-        fprintf(stderr,"%s <collection point> <marquise namespace> [<filename of ip networks to track>]\n\n"
-                "e.g.\n\t%s syd1 pmacct\n",
-                argv[0], argv[0]);
-        return 1;
-    }
-    collection_point = argv[1];
-    if (argc > 3) {
-        ip_whitelist = read_ip_whitelist(argv[3]);
-        if (ip_whitelist == NULL) {
-            if (errno == 0)
-                fprintf(stderr, "invalid or empty ip whitelist file\n");
-            return 1;
-        }
-    }
+	sd_hashes = g_tree_new(hash_comp);
+	if (argc < 3) {
+		fprintf(stderr,"%s <collection point> <marquise namespace> [<filename of ip networks to track>]\n\n"
+				"e.g.\n\t%s syd1 pmacct\n",
+				argv[0], argv[0]);
+		return 1;
+	}
+	collection_point = argv[1];
+	if (argc > 3) {
+		ip_whitelist = read_ip_whitelist(argv[3]);
+		if (ip_whitelist == NULL) {
+			if (errno == 0)
+				fprintf(stderr, "invalid or empty ip whitelist file\n");
+			return 1;
+		}
+	}
 
-    /* get a new consumer we can send frames to
-     */
-    ctx = marquise_init(argv[2]);
-    if (ctx == NULL) {
-        perror("marquise_init"); return 1;
-    }
+	/* get a new consumer we can send frames to
+	 */
+	ctx = marquise_init(argv[2]);
+	if (ctx == NULL) {
+		perror("marquise_init"); return 1;
+	}
 
-    last_timestamp = timestamp_now();
+	last_timestamp = timestamp_now();
 
-    infile = stdin; /* slack */
+	infile = stdin; /* slack */
 
-    int retcode = 0;
-    while ( fgets(buf, BUFSIZ, infile) == buf ) {
-        /* Ignore any line that doesn't start with a numeric
-         * ID. This gets around pmacct's stupid logging of
-         * totally unimportant warnings to stdout
-         */
-        if (buf[0] < '0' || buf[0] > '9')  continue;
+	int retcode = 0;
+	while ( fgets(buf, BUFSIZ, infile) == buf ) {
+		/* Ignore any line that doesn't start with a numeric
+		 * ID. This gets around pmacct's stupid logging of
+		 * totally unimportant warnings to stdout
+		 */
+		if (buf[0] < '0' || buf[0] > '9') continue;
 
-        /* Keep timestamp the same for all items based on the same entry
-         * in case we need to cross correlate them later
-         */
-        timestamp = timestamp_now();
+		/* Keep timestamp the same for all items based on the same entry
+		 * in case we need to cross correlate them later
+		 */
+		timestamp = timestamp_now();
 
-        /* the 2tuple of source,timestamp must be unique for each frame
-         *
-         * Check to make sure that if we have a really course clock that we
-         * are still maintaining that invariant
-         */
-        if (timestamp < last_timestamp)
-            timestamp = last_timestamp + 1;   /* Great. NTP skew. My lucky day */
-        if (timestamp == last_timestamp)
-            timestamp++;
-        last_timestamp = timestamp;
+		/* the 2tuple of source,timestamp must be unique for each frame
+		 *
+		 * Check to make sure that if we have a really course clock that we
+		 * are still maintaining that invariant
+		 */
+		if (timestamp < last_timestamp)
+			timestamp = last_timestamp + 1; /* Great. NTP skew. My lucky day */
+		if (timestamp == last_timestamp)
+			timestamp++;
+		last_timestamp = timestamp;
 
-        buf[BUFSIZ-1] = 0;
-        if (! parse_pmacct_record(buf, &source_ip, &dest_ip, &bytes))
-            continue; /* Doesn't look like it's actually a record */
+		buf[BUFSIZ-1] = 0;
+		if (! parse_pmacct_record(buf, &source_ip, &dest_ip, &bytes))
+			continue; /* Doesn't look like it's actually a record */
 
-        /* emit a frame for both parties (if the ip is whitelisted) */
-        if (is_address_in_whitelist(source_ip, ip_whitelist) == 1) {
-            if ( emit_tx_bytes(ctx, collection_point, source_ip, timestamp, bytes) != 0 ) {
-                perror(__FILEPOS__ ": marquise_send_simple"); retcode=1; break;
-            }
-        }
-        if (is_address_in_whitelist(dest_ip, ip_whitelist) == 1) {
-            if ( emit_rx_bytes(ctx, collection_point, dest_ip, timestamp, bytes) != 0 ) {
-                perror(__FILEPOS__ ": marquise_send_simple"); retcode=1; break;
-            }
-        }
+		/* emit a frame for both parties (if the ip is whitelisted) */
+		if (is_address_in_whitelist(source_ip, ip_whitelist) == 1) {
+			if ( emit_tx_bytes(ctx, collection_point, source_ip, timestamp, bytes) != 0 ) {
+				perror(__FILEPOS__ ": marquise_send_simple"); retcode=1; break;
+			}
+		}
+		if (is_address_in_whitelist(dest_ip, ip_whitelist) == 1) {
+			if ( emit_rx_bytes(ctx, collection_point, dest_ip, timestamp, bytes) != 0 ) {
+				perror(__FILEPOS__ ": marquise_send_simple"); retcode=1; break;
+			}
+		}
 
-        free(source_ip);
-        free(dest_ip);
-    }
+		free(source_ip);
+		free(dest_ip);
+	}
 
-    marquise_shutdown(ctx);
-    free_whitelist(ip_whitelist);
+	marquise_shutdown(ctx);
+	free_whitelist(ip_whitelist);
 
-    return retcode;
+	return retcode;
 }
--- src/util.c
+++ src/util.c
@@ -9,72 +9,72 @@
 #include "util.h"
 
 marquise_source *build_marquise_source(char *collection_point, char *ip, const char *bytes) {
-    size_t n_tags = SOURCE_NUM_TAGS;
-    char **fields = malloc(sizeof(char *) * n_tags);
-    char **values = malloc(sizeof(char *) * n_tags);
-    fields[0] = strdup(SOURCE_KEY_BYTES);
-    values[0]   = strdup(bytes);
-    fields[1] = strdup(SOURCE_KEY_COLLECTION_POINT);
-    values[1]   = strdup(collection_point);
-    fields[2] = strdup(SOURCE_KEY_IP);
-    values[2]   = strdup(ip);
-    return marquise_new_source(fields, values, n_tags);
+	size_t n_tags = SOURCE_NUM_TAGS;
+	char **fields = malloc(sizeof(char *) * n_tags);
+	char **values = malloc(sizeof(char *) * n_tags);
+	fields[0] = strdup(SOURCE_KEY_BYTES);
+	values[0]   = strdup(bytes);
+	fields[1] = strdup(SOURCE_KEY_COLLECTION_POINT);
+	values[1]   = strdup(collection_point);
+	fields[2] = strdup(SOURCE_KEY_IP);
+	values[2]   = strdup(ip);
+	return marquise_new_source(fields, values, n_tags);
 }
 
 char *serialise_marquise_source(marquise_source *marq_source) {
-    char **fields = marq_source->fields;
-    char **values = marq_source->values;
-    int n = marq_source->n_tags;
-    int length = 1; //NULL byte at end
-    unsigned int i;
-    for (i = 0; i < n; i++) {
-        length += strlen(fields[i]);
-        length += strlen(values[i]);
-    }
-    char *str = malloc(sizeof(char) * length);
-    i = 0;
-    while (i < length) {
-        strcpy(str + i, fields[i]);
-        i += strlen(fields[i]);
-        strcpy(str + i, values[i]);
-        i += strlen(values[i]);
-    } 
-    str[i] = '\0';
-    return str;
+	char **fields = marq_source->fields;
+	char **values = marq_source->values;
+	int n = marq_source->n_tags;
+	int length = 1; //NULL byte at end
+	unsigned int i;
+	for (i = 0; i < n; i++) {
+		length += strlen(fields[i]);
+		length += strlen(values[i]);
+	}
+	char *str = malloc(sizeof(char) * length);
+	i = 0;
+	while (i < length) {
+		strcpy(str + i, fields[i]);
+		i += strlen(fields[i]);
+		strcpy(str + i, values[i]);
+		i += strlen(values[i]);
+	} 
+	str[i] = '\0';
+	return str;
 }
 
 char *build_address_string(char *collection_point, char *ip, const char *bytes) {
-    /* order matters */
-    size_t source_len = sizeof(SOURCE_KEY_BYTES) +
-                        sizeof(SOURCE_KEY_COLLECTION_POINT) +
-                        sizeof(SOURCE_KEY_IP); 
-    size_t bytes_len = strlen(bytes);
-    size_t collection_point_len = strlen(collection_point);
-    size_t ip_len = strlen(ip);
-    /* the +2 is for the ',' and ':' */
-    source_len += bytes_len + 2;
-    source_len += collection_point_len + 2;
-    source_len += ip_len + 2;
-    source_len += 1; // NULL
-    char *source = malloc(source_len);
-    strcpy(source, SOURCE_KEY_BYTES);
-    int idx = sizeof(SOURCE_KEY_BYTES) - 1;
-    source[idx++] = ':';
-    strcpy(source + idx, bytes);
-    idx += bytes_len;
-    source[idx++] = ',';
-    strcpy(source + idx, SOURCE_KEY_COLLECTION_POINT);
-    idx += sizeof(SOURCE_KEY_COLLECTION_POINT) - 1;
-    source[idx++] = ':';
-    strcpy(source + idx, collection_point);
-    idx += collection_point_len;
-    source[idx++] = ',';
-    strcpy(source + idx, SOURCE_KEY_IP);
-    idx += sizeof(SOURCE_KEY_IP) - 1;
-    source[idx++] = ':';
-    strcpy(source + idx, ip);
-    idx += ip_len;
-    source[idx++] = ',';
-    source[idx++] = '\0';
-    return source;
+	/* order matters */
+	size_t source_len = 	sizeof(SOURCE_KEY_BYTES) +
+				sizeof(SOURCE_KEY_COLLECTION_POINT) +
+				sizeof(SOURCE_KEY_IP); 
+	size_t bytes_len = strlen(bytes);
+	size_t collection_point_len = strlen(collection_point);
+	size_t ip_len = strlen(ip);
+	/* the +2 is for the ',' and ':' */
+	source_len += bytes_len + 2;
+	source_len += collection_point_len + 2;
+	source_len += ip_len + 2;
+	source_len += 1; // NULL
+	char *source = malloc(source_len);
+	strcpy(source, SOURCE_KEY_BYTES);
+	int idx = sizeof(SOURCE_KEY_BYTES) - 1;
+	source[idx++] = ':';
+	strcpy(source + idx, bytes);
+	idx += bytes_len;
+	source[idx++] = ',';
+	strcpy(source + idx, SOURCE_KEY_COLLECTION_POINT);
+	idx += sizeof(SOURCE_KEY_COLLECTION_POINT) - 1;
+	source[idx++] = ':';
+	strcpy(source + idx, collection_point);
+	idx += collection_point_len;
+	source[idx++] = ',';
+	strcpy(source + idx, SOURCE_KEY_IP);
+	idx += sizeof(SOURCE_KEY_IP) - 1;
+	source[idx++] = ':';
+	strcpy(source + idx, ip);
+	idx += ip_len;
+	source[idx++] = ',';
+	source[idx++] = '\0';
+	return source;
 }
